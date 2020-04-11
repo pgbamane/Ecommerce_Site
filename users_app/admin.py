01096d06d17ed5856f93466c1a6be13cdef00183
pgbamane <pradnya.bamane23@gmail.com>
2020-04-11 16:47:42 +0530

    test for create_user manager done

--- users_app/admin.py
+++ users_app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-# from users_app.models import User
-#
-# admin.register(User)
+from users_app.models import User
+
+admin.site.register(User)
--- users_app/tests.py
+++ users_app/tests.py
@@ -1,1 +1,18 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+
+
+class UserManagerTestCase(TestCase):
+
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(first_name='Pradnya',
+                                        last_name='Bamane',
+                                        email_id='pradnya@gmail.com',
+                                        phone_number='9545065515',
+                                        gender='female',
+                                        )
+        self.assertEqual(user.first_name, 'Pradnya')
+        self.assertEqual(user.last_name, 'Bamane')
+        self.assertEqual(user.email_id, 'pradnya@gmail.com')
+        self.assertEqual(user.get_username(), user.email_id)
