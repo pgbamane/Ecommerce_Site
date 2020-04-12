e44804f487e7277c9782d34b48989a2de23f186d
pgbamane <pradnya.bamane23@gmail.com>
2020-04-12 11:08:23 +0530

    user_model forms created

---
+++ users_app/forms.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import User
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm):
+        model = User
+        # fields = ('first_name', 'last_name', 'gender', 'address', 'locality', 'state', )
+        exclude = ('date_joined', 'is_active', 'is_staff', 'is_superuser')
+
+
+class CustomeUserChangeForm(UserChangeForm):
+    class Meta(UserCreationForm):
+        model = User
+        # fields = ('first_name', 'last_name', 'gender', 'address', 'locality', 'state', )
+        exclude = ('date_joined', 'is_active', 'is_staff', 'is_superuser')
